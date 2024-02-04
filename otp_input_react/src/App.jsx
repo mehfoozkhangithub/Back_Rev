f39e4b18b2808a0cf964d061c083f5525312dd8f
unit-2_Program <100725778+mehfoozkhangithub@users.noreply.github.com>
2024-02-05 00:00:55 +0530

    code of otp tailwind setup

---
+++ otp_input_react/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <h1 className="uppercase border-2 border-dashed border-red-700 text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
